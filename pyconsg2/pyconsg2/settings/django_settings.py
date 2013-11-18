3b6c9647145bee706eb7d2f1f26dc021e6d596e5
Martin Brochhaus <mbrochh@gmail.com>
2013-11-18 08:04:23 +0800

    Added review templates

--- pyconsg2/pyconsg2/settings/django_settings.py
+++ pyconsg2/pyconsg2/settings/django_settings.py
@@ -79,6 +79,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.static',
     'cms.context_processors.media',
     'sekizai.context_processors.sekizai',
+    'symposion.reviews.context_processors.reviews',
 )
 
 MIDDLEWARE_CLASSES = (
