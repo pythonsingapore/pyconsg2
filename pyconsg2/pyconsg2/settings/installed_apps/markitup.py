2bf2e4f0b14f77564060d92515a4df0c90418991
Martin Brochhaus <mbrochh@gmail.com>
2014-01-12 15:42:55 +0800

    Set JQUERY_URL to None for moarkitup

--- pyconsg2/pyconsg2/settings/installed_apps/markitup.py
+++ pyconsg2/pyconsg2/settings/installed_apps/markitup.py
@@ -1,5 +1,6 @@
 """Settings for the ``markitup`` app."""
 
+JQUERY_URL = None
 MARKITUP_FILTER = ('markdown.markdown', {'safe_mode': True})
 MARKITUP_SET = 'markitup/sets/markdown'
 MARKITUP_SKIN = 'markitup/skins/simple'
