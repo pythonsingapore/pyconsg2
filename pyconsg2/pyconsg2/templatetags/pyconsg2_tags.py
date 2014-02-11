e0565fa6f604518c99d45b219df5042607b04028
Martin Brochhaus <mbrochh@gmail.com>
2014-02-11 21:07:08 +0800

    Found better way to render magic placeholder

--- pyconsg2/pyconsg2/templatetags/pyconsg2_tags.py
+++ pyconsg2/pyconsg2/templatetags/pyconsg2_tags.py
@@ -1,6 +1,7 @@
 """Templatetags for the pyconsg2 project."""
 from django import template
 from django.template import Template
+from django.template.defaultfilters import safe
 
 from classytags.arguments import Argument, MultiValueArgument
 from cms.templatetags.cms_tags import Placeholder, PlaceholderOptions
@@ -13,18 +14,6 @@ register = template.Library()
 def inspect(obj):
     import ipdb; ipdb.set_trace() # BREAKPOINT
     return obj
-
-
-@register.assignment_tag(takes_context=True)
-def render_template(context, template_string):
-    """
-    Returns the given content string as a rendered Django template.
-
-    :param template_string: A string that follows the Django template syntax.
-
-    """
-    template = Template(template_string)
-    return template.render(context)
 
 
 class PlaceholderAs(Placeholder):
@@ -42,8 +31,10 @@ class PlaceholderAs(Placeholder):
     def render_tag(self, context, name, extra_bits, varname):
         output = super(PlaceholderAs, self).render_tag(
             context, name, extra_bits)
+        output_template = Template(safe(output))
+        output_rendered = output_template.render(context)
         if varname:
-            context[varname] = output
+            context[varname] = output_rendered
             return ''
-        return output
+        return output_rendered
 register.tag(PlaceholderAs)
