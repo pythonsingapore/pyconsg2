3c597405616b4d8b73a13a9efb730519a0a0430a
Martin Brochhaus <mbrochh@gmail.com>
2014-05-08 04:44:40 +0800

    Added ticket counter

--- pyconsg2/pyconsg2/templatetags/pyconsg2_tags.py
+++ pyconsg2/pyconsg2/templatetags/pyconsg2_tags.py
@@ -2,6 +2,7 @@
 from django import template
 
 from cms.models.static_placeholder import StaticPlaceholder
+from paypal_express_checkout.models import PurchasedItem
 
 
 register = template.Library()
@@ -17,3 +18,14 @@ def inspect(obj):
 @register.assignment_tag
 def get_static_placeholders():
     return StaticPlaceholder.objects.all()
+
+
+@register.assignment_tag
+def get_early_bird_count(ticket_amount):
+    count = ticket_amount - PurchasedItem.objects.filter(
+        transaction__status='completed',
+        item__identifier__in=['conference-early', 'conference-student-early', ],
+    ).count()
+    if count < 0:
+        count = 0
+    return count
