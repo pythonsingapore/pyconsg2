a6e126d0015599e410e27b5ad02a463a8c91219b
Martin Brochhaus <mbrochh@gmail.com>
2015-05-19 19:00:40 +0800

    Given a more generic sale description

--- pyconsg2/pyconsg2/settings/installed_apps/paypal_express_checkout.py
+++ pyconsg2/pyconsg2/settings/installed_apps/paypal_express_checkout.py
@@ -1,4 +1,4 @@
 """Settings for the ``paypal_express_checkout`` app."""
 HOSTNAME = 'https://pycon.sg'
-SALE_DESCRIPTION = 'Your payment for PyCon Singapore 2015'
+SALE_DESCRIPTION = 'Your payment for PyCon Singapore'
 PAYPAL_SET_CHECKOUT_FORM = 'paypal_pyconsg.forms.PyconsgSetExpressCheckoutForm'
